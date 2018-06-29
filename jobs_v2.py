25da117159b5cb2f83ea99eee5efb18f4212a565
Ruotian Luo <>
2018-06-29 16:30:52 -0700

    Avoid downloading duplicate tgz files. Save all the weight files in a folder.

--- jobs_v2.py
+++ jobs_v2.py
@@ -1,7 +1,11 @@
 import os
+os.system('mkdir -p mobilenet_v2_weights')
 for mt in ['1.0', '0.75', '0.5', '0.35']:
     for sz in [224, 192, 160, 128]:
-        cmd = 'wget https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_%s_%d.tgz;'%(mt,sz)
-        cmd += 'tar -xf mobilenet_v2_%s_%d.tgz;'%(mt,sz)
-        cmd += 'python convert_v2.py --depth_multiplier %s --image_size %d --tensorflow_model  mobilenet_v2_%s_%d.ckpt'%(mt,sz,mt,sz)
+        if os.path.isfile('mobilenet_v2_weights/mobilenet_v2_%s_%d.tgz'%(mt,sz)):
+            cmd = ''
+        else:
+            cmd = 'wget -P mobilenet_v2_weights https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_%s_%d.tgz;'%(mt,sz)
+        cmd += 'tar -xf mobilenet_v2_weights/mobilenet_v2_%s_%d.tgz -C mobilenet_v2_weights;'%(mt,sz)
+        cmd += 'python convert_v2.py --depth_multiplier %s --image_size %d --tensorflow_model  mobilenet_v2_weights/mobilenet_v2_%s_%d.ckpt'%(mt,sz,mt,sz)
         os.system(cmd)
